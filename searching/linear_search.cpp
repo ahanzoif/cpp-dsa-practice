36855941094dad62e2ac0b9a1b6455c107161a9f
Ahan Zoif | Md Kaif <kaifmd9006@gmail.com>
2026-03-10 00:01:29 +0530

    Add linear search implementation in C++
    Implements a linear search algorithm to find the index of a target element in an array.

---
+++ searching/linear_search.cpp
@@ -0,0 +1,31 @@
+// Program: Linear Search in Array
+// Topic: Searching Algorithms
+// Language: C++
+// Author: Ahan Zoif
+// Description: Finds the index of a target element by traversing the array sequentially.
+
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target) // FOUND
+        {
+            return i;
+        }
+    }
+
+    return -1; // NOT FOUND
+}
+
+int main()
+{
+    int arr[] = {4, 2, 7, 8, 1, 2, 5};
+    int size = 7;
+    int target = 8;
+
+    cout << linearSearch(arr, size, target) << endl;
+    return 0;
+}
